6f41c35ab212062ac5780d9ff196cb47a5c8f051
“JazlynL” <lubenjazlyn35@gmail.com>
2022-01-17 17:44:24 -0500

    datatype notes

--- src/com/careerdevs/intro/DataTypes.java
+++ src/com/careerdevs/intro/DataTypes.java
@@ -2,6 +2,9 @@ package com.careerdevs.intro;
 
 public class DataTypes {
     public static void main(String[] args) {
-
-    }
+  // primitive types for storing simple values
+        //reference types for storing more complex values
+        // we can use a under score instead of a comma when storing values
+        // so when we are using Char characters we will use single quotes when identifying a string we use double quotes
+ // reference types are copied by the reference while primitive types are copied by the value    }
 }
